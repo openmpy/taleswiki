fa642cfd95d0afbb870401c577cb0617c39e4530
openmpy <suhwan@kakao.com>
2025-05-04 03:07:15 +0900

    init commit

---
+++ server/src/main/java/com/openmpy/taleswiki/TaleswikiApplication.java
@@ -0,0 +1,13 @@
+package com.openmpy.taleswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleswikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaleswikiApplication.class, args);
+	}
+
+}
