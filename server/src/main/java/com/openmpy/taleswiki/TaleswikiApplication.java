6a748a8d2919e322d03b61078a257a93e007a54c
openmpy <suhwan@kakao.com>
2025-05-04 03:21:42 +0900

    style: 탭 사이즈 수정

--- server/src/main/java/com/openmpy/taleswiki/TaleswikiApplication.java
+++ server/src/main/java/com/openmpy/taleswiki/TaleswikiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaleswikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaleswikiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaleswikiApplication.class, args);
+    }
 
 }
