9496d92766d90efd3c1ad2c5a2335f495e68afa8
openmpy <suhwan@kakao.com>
2025-05-05 17:15:16 +0900

    feat: Cors 설정

---
+++ server/src/main/java/com/openmpy/taleswiki/common/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.openmpy.taleswiki.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600L);
+    }
+}
