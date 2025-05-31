a8a2815a9e3482c602bdb77a610c08bc42b21916
openmpy <suhwan@kakao.com>
2025-05-31 13:43:25 +0900

    refactor: 푸터 수정

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FaDiscord, FaEnvelope } from "react-icons/fa";
-import { RiKakaoTalkFill } from "react-icons/ri";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
@@ -9,22 +8,13 @@ function Footer() {
     <footer className="bg-gray-800 text-white p-3 text-center mt-auto border-t border-gray-700 text-sm">
       <div className="flex justify-center items-center space-x-3 mb-2">
         <a
-          href="https://discord.gg/eXEtzDEk"
+          href="https://discord.gg/gejJjkvFpq"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-blue-400"
           aria-label="Discord 서버로 이동"
         >
           <FaDiscord size={18} />
-        </a>
-        <a
-          href="https://open.kakao.com/o/gbPFwsvh"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-blue-400"
-          aria-label="카카오톡 오픈채팅으로 이동"
-        >
-          <RiKakaoTalkFill size={18} />
         </a>
         <a
           href="mailto:taleswiki@proton.me"
