1a6d3fe3c0636869b88c0c3de4e40d3263b6d115
openmpy <suhwan@kakao.com>
2025-05-31 15:03:38 +0900

    refactor: SEO 개선

--- client/src/components/SEO.jsx
+++ client/src/components/SEO.jsx
@@ -5,8 +5,8 @@ const SEO = ({ title, description, keywords, ogImage, ogUrl }) => {
   const defaultDescription =
     "테일즈위키는 공익성을 가지는 커뮤니티입니다. 누구나 문서를 조회, 작성, 편집할 수 있습니다.";
   const defaultKeywords = "테일즈런너, 테일즈위키, 게임위키, 게임정보";
-  const defaultOgImage = "https://taleswiki.shop/og-image.png";
-  const defaultOgUrl = "https://taleswiki.shop";
+  const defaultOgImage = "https://taleswiki.com/og-image.png";
+  const defaultOgUrl = "https://taleswiki.com";
 
   return (
     <Helmet>
