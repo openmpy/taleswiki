9ba20a194ec1c5d942923a058be8ba05403c9c98
openmpy <suhwan@kakao.com>
2025-06-20 21:36:04 +0900

    chore: 커뮤니티 페이지 추가

---
+++ client/src/pages/CommunityPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CommunityPage() {
+  return (
+    <div className="flex justify-center items-center min-h-[400px]">
+      <div className="text-gray-500 font-medium">
+        커뮤니티가 오픈될 예정입니다.
+      </div>
+    </div>
+  );
+}
+
+export default CommunityPage;
