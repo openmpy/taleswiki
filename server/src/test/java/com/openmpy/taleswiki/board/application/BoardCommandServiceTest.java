acda47f6e4012e95c62ca07dff6b11997eb5ee94
openmpy <suhwan@kakao.com>
2025-06-30 15:54:14 +0900

    test: 게시글 조회수 증가, 삭제 테스트 코드 추가

--- server/src/test/java/com/openmpy/taleswiki/board/application/BoardCommandServiceTest.java
+++ server/src/test/java/com/openmpy/taleswiki/board/application/BoardCommandServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import com.openmpy.taleswiki.board.domain.entity.Board;
+import com.openmpy.taleswiki.board.domain.repository.BoardRepository;
 import com.openmpy.taleswiki.board.dto.request.BoardSaveRequest;
 import com.openmpy.taleswiki.board.dto.response.BoardSaveResponse;
 import com.openmpy.taleswiki.helper.Fixture;
@@ -23,6 +25,9 @@ class BoardCommandServiceTest extends ServiceTestSupport {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @DisplayName("[통과] 게시글을 작성한다.")
     @Test
     void board_command_service_test_01() {
@@ -40,5 +45,35 @@ class BoardCommandServiceTest extends ServiceTestSupport {
 
         // then
         assertThat(response.boardId()).isNotNull();
+    }
+
+    @DisplayName("[통과] 게시글 조회수를 증가시킨다.")
+    @Test
+    void board_command_service_test_02() {
+        // given
+        final Member member = memberRepository.save(Fixture.createMember());
+        final Board board = boardRepository.save(Board.save("제목", "내용", "테붕이01", "127.0.0.1", member));
+
+        // when
+        boardCommandService.incrementViews(board.getId(), 1L);
+
+        // then
+        final Board first = boardRepository.findAll().getFirst();
+        assertThat(first.getView()).isEqualTo(1L);
+    }
+
+    @DisplayName("[통과] 게시글을 삭제한다.")
+    @Test
+    void board_command_service_test_03() {
+        // given
+        final Member member = memberRepository.save(Fixture.createMember());
+        final Board board = boardRepository.save(Board.save("제목", "내용", "테붕이01", "127.0.0.1", member));
+
+        // when
+        boardCommandService.delete(member.getId(), board.getId());
+
+        // then
+        final long count = boardRepository.count();
+        assertThat(count).isZero();
     }
 }
